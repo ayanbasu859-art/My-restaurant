3cc71938f2708b7135b9dffb897bbd7e53601772
ayanbasu859-art <ayanbasu859@gmail.com>
2026-03-09 17:45:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu(){
+var menu = document.getElementById("menu");
+
+if(menu.style.display === "block"){
+menu.style.display = "none";
+}else{
+menu.style.display = "block";
+}
+}
+
+function toggleDark(){
+  document.body.classList.toggle("dark");
+}
+
+function toggleDark(){
+  document.body.classList.toggle("dark");
+}
